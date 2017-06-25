d6e2f2f3f2215e2ecde71bfff00ae00c52abcc05
Yong Huang <yong.e.huang@gmail.com>
2017-06-25 16:44:37 +0200

    Provide complete solution to Hangman

--- w3_structured_types/solution/ps3_hangman.py
+++ w3_structured_types/solution/ps3_hangman.py
@@ -28,6 +28,7 @@ def loadWords():
     print("  ", len(wordlist), "words loaded.")
     return wordlist
 
+
 def chooseWord(wordlist):
     """
     wordlist (list): list of words (strings)
@@ -36,12 +37,14 @@ def chooseWord(wordlist):
     """
     return random.choice(wordlist)
 
+
 # end of helper code
 # -----------------------------------
 
 # Load the list of words into the variable wordlist
 # so that it can be accessed from anywhere in the program
 wordlist = loadWords()
+
 
 def isWordGuessed(secretWord, lettersGuessed):
     '''
@@ -55,6 +58,7 @@ def isWordGuessed(secretWord, lettersGuessed):
             return False
 
     return True
+
 
 def getGuessedWord(secretWord, lettersGuessed):
     '''
@@ -73,7 +77,6 @@ def getGuessedWord(secretWord, lettersGuessed):
     return guessed_so_far
 
 
-
 def getAvailableLetters(lettersGuessed):
     '''
     lettersGuessed: list, what letters have been guessed so far
@@ -87,7 +90,7 @@ def getAvailableLetters(lettersGuessed):
         available_letters = available_letters.replace(letter, '')
 
     return available_letters
-    
+
 
 def hangman(secretWord):
     '''
@@ -109,16 +112,62 @@ def hangman(secretWord):
 
     Follows the other limitations detailed in the problem write-up.
     '''
-    # FILL IN YOUR CODE HERE...
+    secret_word_lowercase = secretWord.lower()
 
+    welcome_greeting = "Welcome to the game, Hangman!"
+    print(welcome_greeting)
 
+    length_reminder_format = "I am thinking of a word that is {} letters long."
+    print(length_reminder_format.format(len(secret_word_lowercase)))
 
+    split_line = "-------------"
 
+    remaining_guess = 8
+    remaining_guess_reminder_format = "You have {} guesses left."
 
+    available_letters_prefix = 'Available letters:'
+
+    good_guess_prefix = "Good guess:"
+    repeat_guess_prefix = "Oops! You've already guessed that letter:"
+    bad_guess_prefix = "Oops! That letter is not in my word:"
+    won = "Congratulations, you won!"
+    fail = "Sorry, you ran out of guesses. The word was else."
+
+    input_prompt = 'Please guess a letter:'
+
+    letters_guessed = []
+
+    while remaining_guess > 0:
+        print(split_line)
+
+        if isWordGuessed(secret_word_lowercase, letters_guessed):
+            print(won)
+            return
+
+        print(remaining_guess_reminder_format.format(remaining_guess))
+        print(available_letters_prefix, getAvailableLetters(letters_guessed))
+
+        input_letter = input(input_prompt)
+        input_letter_lowercase = input_letter.lower()
+
+        if input_letter_lowercase in letters_guessed:
+            print(repeat_guess_prefix, getGuessedWord(secret_word_lowercase, letters_guessed))
+            continue
+
+        letters_guessed.append(input_letter_lowercase)
+        if input_letter_lowercase in secret_word_lowercase:
+            print(good_guess_prefix, getGuessedWord(secret_word_lowercase, letters_guessed))
+        else:
+            print(bad_guess_prefix, getGuessedWord(secret_word_lowercase, letters_guessed))
+            remaining_guess -= 1
+
+    print(split_line)
+    print(fail)
 
 # When you've completed your hangman function, uncomment these two lines
 # and run this file to test! (hint: you might want to pick your own
 # secretWord while you're testing)
 
-# secretWord = chooseWord(wordlist).lower()
-# hangman(secretWord)
+secretWord = chooseWord(wordlist).lower()
+print("The secret word is:", secretWord)
+hangman(secretWord)
