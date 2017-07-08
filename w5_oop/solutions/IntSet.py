47938b27b1f86e533467c0be4217972048df6b33
Yong Huang <yong.e.huang@gmail.com>
2017-07-08 18:21:00 +0200

    Week 5 oop finger exercise. Impl len() and intersect() of intSet.

---
+++ w5_oop/solutions/IntSet.py
@@ -0,0 +1,52 @@
+class intSet(object):
+    """An intSet is a set of integers
+    The value is represented by a list of ints, self.vals.
+    Each int in the set occurs in self.vals exactly once."""
+
+    def __init__(self):
+        """Create an empty set of integers"""
+        self.vals = []
+
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self""" 
+        if not e in self.vals:
+            self.vals.append(e)
+
+    def member(self, e):
+        """Assumes e is an integer
+           Returns True if e is in self, and False otherwise"""
+        return e in self.vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self
+           Raises ValueError if e is not in self"""
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+
+    def intersect(self, other):
+        """
+        Returns intersection between two intSets.
+        :param other: the other intSet
+        :return: intersection
+        """
+        new_set = intSet()
+        for elem in self.vals:
+            if elem in other.vals:
+                new_set.vals.append(elem)
+
+        return new_set
+
+    def __len__(self):
+        """
+        Returns amount of elements in current set.
+        :return: amount of element in current set.
+        """
+        return len(self.vals)
+
+    def __str__(self):
+        """Returns a string representation of self"""
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
+
